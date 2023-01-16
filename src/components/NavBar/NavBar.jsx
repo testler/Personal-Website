f60bd45391030fb5c76d97a67cf74a060d45244d
testler <joshgarst@me.com>
2023-01-15 23:40:34 -0600

    updates

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,44 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import navBtn from "./navBtn.png"
 import "./NavBar.css";
-import homeImg from "./faciocon.png";
-import navBtn from "./humburger.png"
-import logo from "./logo1.png"
-  
+
 const NavBar = () => {
-  const handleNavButtonClick = () =>{
-    let nav = document.getElementById("nav-menu");
-    nav.classList.toggle("active");
-  }
-  const handleHomeIconClick = () =>{
-    let nav = document.getElementById("nav-menu");
-    nav.classList.remove("active");
-  }
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileDevice(window.innerWidth <= 800);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <div id="nav-bar-bar">
-      <button onClick={handleNavButtonClick} className="nav-button" ><img src={navBtn} alt="navigation button"/></button>
-      <Link onClick={handleHomeIconClick} id="li" to="/">
-        <img src={logo} alt="Joshua Gasrt logo" id="logo"/>
-      </Link>
-    </div>
-      <nav id="nav-menu">
-        <Link onClick={handleNavButtonClick} id="li" to="/">
-          <img className="home-button" src={homeImg} alt="home button" />
-          <a>Home</a>
-        </Link>
-        <Link onClick={handleNavButtonClick} id="li" to="/about">
-          About
-        </Link>
-        <Link onClick={handleNavButtonClick} id="li" to="/projects">
-          Projects
-        </Link>
-        <Link onClick={handleNavButtonClick} id="li" to="/resume">
-          Resume
-        </Link>
-        <Link onClick={handleNavButtonClick} id="li" to="/contactMe">
-          Contact Me
-        </Link>
+      <nav className={`nav-menu ${isOpen || !isMobileDevice ? "active" : ""}`}>
+        <div className={`nav-bar-bar ${isMobileDevice ? "mobile" : ""}`}>
+          {isMobileDevice && (
+            <button onClick={toggleNav} className={`nav-button ${isOpen ? "" : "open"}`}>
+              <img src={navBtn} alt="nav button" />
+            </button>
+          )}
+        </div>
+        <div className={`nav-links ${isOpen ? "active" : ""}`}>
+          <Link onClick={isMobileDevice ? toggleNav : null} to="/">
+            Home
+          </Link>
+          <Link onClick={isMobileDevice ? toggleNav : null} to="/about">
+            About
+          </Link>
+          <Link onClick={isMobileDevice ? toggleNav : null} to="/projects">
+            Projects
+          </Link>
+          <Link onClick={isMobileDevice ? toggleNav : null} to="/resume">
+            Resume
+          </Link>
+          <Link onClick={isMobileDevice ? toggleNav : null} to="/contactMe">
+            Contact Me
+          </Link>
+        </div>
       </nav>
     </>
   );
