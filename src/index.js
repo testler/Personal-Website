4792214cd5ad682fcd8ef61fde379df149e9eaca
Joshua Garst <joshgarst@me.com>
2023-09-17 23:56:54 -0500

    PWA implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from "react";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,3 +16,5 @@ root.render(
     </React.StrictMode>
   </Router>
 );
+serviceWorkerRegistration.register();
+reportWebVitals();
