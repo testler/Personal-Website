46e4865fbcf4f32fb9be7d323a1005786d0cc0cc
Joshua Garst <joshgarst@me.com>
2025-01-09 03:46:28 -0600

    Updated Site Info

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet";
 import p1Image from "./cyberdefense.png";
 import p2Image from "./datenightorganizer.png";
 import p3Image from "./movieApp.png";
-import p4Image from "./AllThingsAV.jpg";
+import p4Image from "./the-cube.png";
 
 const Projects = () => {
   const [projectList, setProjectList] = useState([
@@ -20,7 +20,7 @@ const Projects = () => {
     {
       projectName: "Date Night Organizer",
       url: "https://github.com/testler/DateNightOrganizer",
-      deploymentUrl: "https://datenightorganizer.herokuapp.com/",
+      deploymentUrl: "https://datenightorganizer.netlify.app/resume",
       image: p2Image,
       description:
       "Date Night Organizer is a service-oriented web app to inspire couples. Used Express.js, Node.js, and EJS for server-side rendering, MongoDB, Mongoose, and EJS for database management. Utilized Git, GitHub, and Agile methodologies for efficient and effective management of the project. Aimed to provide personalized, tailored date ideas for users using responsive design for optimal user experience across devices.",
@@ -28,18 +28,18 @@ const Projects = () => {
     {
       projectName: "The Movie App",
       url: "https://github.com/testler/movieApp",
-      deploymentUrl: "https://runtimeterror1.herokuapp.com/",
+      deploymentUrl: "",
       image: p3Image,
       description:
-        "The Movie App, was developed with team of 4 engineers. Utilized React.js, Node.js, IMDB API, MongoDB for seamless and intuitive movie discovery. Developer used Agile, Git, GitHub, Trello, Slack for efficient and effective management, collaboration, and received positive feedback from users."
+        "(currently unavaible due to problems with hosting servers) The Movie App, was developed with team of 4 engineers. Utilized React.js, Node.js, IMDB API, MongoDB for seamless and intuitive movie discovery. Developer used Agile, Git, GitHub, Trello, Slack for efficient and effective management, collaboration, and received positive feedback from users."
           },
     {
-      projectName: "All Things By AV",
-      url: "https://github.com/testler/AllyvegaStoreFrontend",
-      deploymentUrl: "WIP",
+      projectName: "The Cube Experiment",
+      url: "https://github.com/testler/the-cube/tree/main/the_cube",
+      deploymentUrl: "https://thecubeexperiment.netlify.app/",
       image: p4Image,
       description:
-        "All Things AV is an e-commerce storefront that uses Angular and Java Spring Boot. Implemented endpoints protection, used PostgreSQL for data storage. Utilized Angular for UI/UX, delivered project on time, budget, exceeded client expectations."    },
+        "This is an experiment in 3D web design using react, I thought is would be cool to create an 3d navigational interface"    },
   ]);
   return (
     <section id="outlet">
